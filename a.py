4ab08fc333c9087b8b9628603fee85a23063a92a
Nekkit06 <nickkita00@icloud.com>
2021-09-11 17:32:50 +0300

    add A task

---
+++ a.py
@@ -0,0 +1,15 @@
+import math
+import sys
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
+# Ввод, если два числа и более в одну строку
+(A, B, C) = [int(s) for s in input().split()]
+(H, L) = [int(s) for s in input().split()]
+n=(A*B+B*C+A*C)*2-A*B
+n=n-0.15*n
+m=H*0.001
+p=L*0.001
+j=m*p
+j=j-0.1*j
+u=n/j
+print(math.ceil(u))
